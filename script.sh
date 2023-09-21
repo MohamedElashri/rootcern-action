7087060d2bc9f7c311c8d661c85e8e5f67a57e7d
Mohamed Elashri <muhammadelashri@gmail.com>
2023-09-21 12:38:07 -0400

    new code

--- script.sh
+++ script.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
-# Capture input parameters
-ROOT_SCRIPT=$1
-OUTPUT_FILE=$2
-
-# Setup the ROOT environment
 export PATH=$PATH:/usr/local/root/bin
-export ROOTSYS=/usr/local/root
-export CMAKE_PREFIX_PATH=/usr/local/root
-export LD_LIBRARY_PATH=/usr/local/root/lib
 
-# Compile and run the ROOT script
-root -b -q -l $ROOT_SCRIPT
+# Split the scripts and output files into arrays
+IFS=',' read -ra SCRIPTS <<< "\$1"
+IFS=',' read -ra OUTPUT_FILES <<< "\$2"
 
-# Check if the output file exists, if specified
-if [ -n "$OUTPUT_FILE" ]; then
-  if [ -f $OUTPUT_FILE ]; then
-    echo "ROOT script executed successfully, and the output file exists."
+# Loop over the scripts and output files
+for i in "${!SCRIPTS[@]}"; do
+  # Compile the ROOT script
+  root -b -q -l ${SCRIPTS[$i]}
+  
+  # Check the exit status of the ROOT command
+  if [ $? -eq 0 ]; then
+    echo "ROOT script ${SCRIPTS[$i]} executed successfully."
   else
-    echo "Error: ROOT script did not generate the specified output file."
+    echo "Error: ROOT script ${SCRIPTS[$i]} failed to execute."
     exit 1
   fi
-else
-  echo "ROOT script executed successfully. No output file specified to check for."
-fi
+
+  # If an output file is specified, check if it exists
+  if [ ! -z "${OUTPUT_FILES[$i]}" ]; then
+    if [ -f ${OUTPUT_FILES[$i]} ]; then
+      echo "ROOT script ${SCRIPTS[$i]} generated the output file ${OUTPUT_FILES[$i]} successfully."
+    else
+      echo "Warning: ROOT script ${SCRIPTS[$i]} did not generate the output file ${OUTPUT_FILES[$i]}."
+    fi
+  fi
+done
